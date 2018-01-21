d10c4ea3f0263e62abe4882900bdf04c21055545
PatrykRudnicki <patrudnicki@gmail.com>
2018-01-21 12:58:55 +0100

    Part 1 of tutorial

---
+++ src/main/scala/com/lightbend/akka/sample/AkkaStartStopExperiments.scala
@@ -0,0 +1,40 @@
+package com.lightbend.akka.sample
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class StartStopActor1 extends Actor {
+  override def preStart(): Unit = {
+    println("first started")
+    context.actorOf(Props[StartStopActor2], "second")
+  }
+  override def postStop(): Unit = println("first stopped")
+
+  override def receive: Receive = {
+    case "stop" ⇒ context.stop(self)
+  }
+}
+
+class StartStopActor2 extends Actor {
+  override def preStart(): Unit = println("second started")
+  override def postStop(): Unit = println("second stopped")
+
+  // Actor.emptyBehavior is a useful placeholder when we don't
+  // want to handle any messages in the actor.
+  override def receive: Receive = Actor.emptyBehavior
+}
+
+object ActorStartStopExperiments extends App {
+  val system = ActorSystem("startStopSystem")
+
+  println("Before system")
+
+  val first = system.actorOf(Props[StartStopActor1], "first")
+
+  println("I'm here")
+
+  first ! "stop"
+
+  println("After all")
+
+  system.terminate()
+}
---
+++ src/main/scala/com/lightbend/akka/sample/AkkaSupervisingExperiments.scala
@@ -0,0 +1,32 @@
+package com.lightbend.akka.sample
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class SupervisingActor extends Actor {
+  val child = context.actorOf(Props[SupervisedActor], "supervised-actor")
+
+  override def receive: Receive = {
+    case "failChild" ⇒ child ! "fail"
+  }
+}
+
+class SupervisedActor extends Actor {
+  override def preStart(): Unit = println("supervised actor started")
+  override def postStop(): Unit = println("supervised actor stopped")
+
+  override def receive: Receive = {
+    case "fail" ⇒
+      println("supervised actor fails now")
+      throw new Exception("I failed!")
+  }
+}
+
+object ActorSupervisingExperiments extends App{
+  val system = ActorSystem("supervising")
+
+  val supervisingActor = system.actorOf(Props[SupervisingActor], "supervising-actor")
+
+  supervisingActor ! "failChild"
+
+  system.terminate()
+}
